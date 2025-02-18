936e1deec14b75aa26e4827f93056e87cf7d6bf5
kae-mi <rudahyang7815@gmail.com>
2025-02-18 15:32:54 +0900

    section1 수강 완료

---
+++ springsecsection1/src/main/java/com/springsecurity/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Spring Security Application With Spring Security";
+    }
+}
