8c7a75b6298a96bb5162117180bff50d8ecceb5e
kae-mi <rudahyang7815@gmail.com>
2025-03-15 20:59:43 +0900

    section4 수강 완료

---
+++ springsecsection4/src/main/java/com/springsecurity/springsecsection4/controller/NoticesController.java
@@ -0,0 +1,14 @@
+package com.springsecurity.springsecsection4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoticesController {
+
+    @GetMapping("/notices")
+    public String getNotices() {
+
+        return "Here is the notices details";
+    }
+}
