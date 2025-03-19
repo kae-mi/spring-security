8839d35472126c768ad6829bac65b3102c84ab1c
kae-mi <rudahyang7815@gmail.com>
2025-03-19 18:04:10 +0900

    section6 학습 완료

---
+++ springsecsection6/src/main/java/com/springsecurity/springsecsection6/config/ProjectUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.springsecurity.springsecsection6.config;
+
+import com.springsecurity.springsecsection6.model.Customer;
+import com.springsecurity.springsecsection6.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+    /**
+     * @param username the username identifying the user whose data is required.
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPwd(), authorities);
+    }
+}
---
+++ springsecsection6/src/main/java/com/springsecurity/springsecsection6/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecsection6.repository;
+
+import com.springsecurity.springsecsection6.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+}
