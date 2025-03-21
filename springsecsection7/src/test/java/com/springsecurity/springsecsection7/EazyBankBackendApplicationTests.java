0899891fc248e8b6af7249ad6c84240dcd02ad20
kae-mi <rudahyang7815@gmail.com>
2025-03-21 18:36:56 +0900

    section7 학습 완료

---
+++ springsecsection7/src/test/java/com/springsecurity/springsecsection7/EazyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.springsecsection7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
