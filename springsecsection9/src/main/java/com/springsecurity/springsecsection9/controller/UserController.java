d8037d1e1476df038e276736a9ddfb2faa2b15b8
kae-mi <rudahyang7815@gmail.com>
2025-03-24 17:43:21 +0900

    section9 학습 완료

---
+++ springsecsection9/src/main/java/com/springsecurity/springsecsection9/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.springsecurity.springsecsection9.controller;
+
+import com.springsecurity.springsecsection9.model.Customer;
+import com.springsecurity.springsecsection9.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+
+        try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
+            Customer savedCustomer = customerRepository.save(customer);
+
+            if (savedCustomer.getId() > 0) { // 성공적으로 DB에 저장되면
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .body("유저 성공적으로 등록");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("유저 등록 실패");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("예외 발생: " + e.getMessage());
+        }
+    }
+}
